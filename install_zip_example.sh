5d72d72baaf685597b74b0df57a10f43c6a7405a
SHIO777 <shiotatsu1015@gmail.com>
2022-10-29 23:30:26 +0900

    [add]

--- install_zip_example.sh
+++ install_zip_example.sh
@@ -4,4 +4,7 @@ cd ~/example
 # URLからMission Plannerのzipファイルをダウンロード
 wget https://firmware.ardupilot.org/Tools/MissionPlanner/MissionPlanner-latest.zip
 # zipファイルを解凍する
+# 以下の記事より、文字化けを防ぐため、zipファイルの解凍にはunarを使用する。
+# https://qiita.com/supersaiakujin/items/c6b54e9add21d375161f
+sudo apt install unar
 unar MissionPlanner-latest.zip
