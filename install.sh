aa2fe2865153c00b248c102d366ed212dae44cb8
Seigyo Taro <my@google.com>
2022-10-29 23:16:47 +0900

    [add]

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-# https://ardupilot.org/planner/docs/mission-planner-installation.html#mission-planner-on-linux
-# https://www.mono-project.com/download/stable/
-
-sudo apt install gnupg ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
+sudo apt install ros-noetic-desktop-full
+sudo apt install ros-noetic-rqt*
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
 
-# install mono
-sudo apt install mono-devel
-
-mono MissionPlanner/MissionPlanner.exe
