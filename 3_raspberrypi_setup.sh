cfc1e3fe1f4e3ae494d0544f080356e6f3202d92
shio777 <shiotatsu1015@gmail.com>
2025-01-18 00:49:54 +0900

    update: 3raspberry pi

--- 3_raspberrypi_setup.sh
+++ 3_raspberrypi_setup.sh
@@ -20,13 +20,13 @@ sudo apt install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 echo -e "\n apt update \n"
 sudo apt update
-sudo apt install ros-noetic-desktop-full
-sudo apt install ros-noetic-rqt*you
+sudo apt install -y ros-noetic-desktop-full
+sudo apt install -y ros-noetic-rqt*
 source /opt/ros/noetic/setup.bash
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install python3-rosdep
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3-rosdep
 sudo rosdep init
 rosdep update
 echo -e "\n run 'roscore' to confirm ROS proper work \n"
@@ -35,7 +35,8 @@ echo -e "\n run 'roscore' to confirm ROS proper work \n"
 # install MAVROS
 echo -e "\n install mavros ROS plugin \n"
 sudo apt-get install -y ros-noetic-mavros ros-noetic-mavros-extras
-wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh chmod a+x install_geographiclib_datasets.sh 
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+chmod a+x install_geographiclib_datasets.sh 
 sudo ./install_geographiclib_datasets.sh
 
 
